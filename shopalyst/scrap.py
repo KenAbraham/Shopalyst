8397f94ee3252ffcaf49bd9b93f930c02d4c48f9
Ken Abraham <kabraham@menervasoftware.com>
2020-03-09 19:44:39 +0530

    changes

--- shopalyst/scrap.py
+++ shopalyst/scrap.py
@@ -9,15 +9,16 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.implicitly_wait(30)
 driver.maximize_window()
 
+val = input("Enter your product page url ")
 host = "https://www.shoppersstop.com/louis-philippe-mens-printed-formal-shirt/p-206764048/colorChange/206764048_9654"
-driver.get(host)
+driver.get(val)
 time.sleep(4)
 driver.find_element_by_xpath("//button[@class = 'variant-size-button-206764092']").click()
 time.sleep(2)
 
 input_tag = driver.find_element_by_xpath("//input[@id = 'addToCartButton']")
 
-print(input_tag.get_attribute('value'))
+# print(input_tag.get_attribute('value'))
 input_tag.click()
 time.sleep(2)
 driver.find_element_by_xpath("//a[@class = 'minicart bags bag bag-container']").click()
@@ -25,7 +26,7 @@ time.sleep(4)
 
 
 driver.get("https://www.shoppersstop.com/cart")
-print(type(driver.page_source))
+# print(type(driver.page_source))
 
 file1 = open("cart_page.txt","w")
 file1.writelines(driver.page_source) 
